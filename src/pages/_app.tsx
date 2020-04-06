f8391dd8b2336045b90beadb9ebc410549508571
Mathias Lengler <mathias@lengler.dev>
2020-04-06 22:42:10 +0200

    integrate material-ui template
    https://github.com/mui-org/material-ui/tree/master/examples/nextjs-with-typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import {AppProps} from 'next/app'
 import React from "react";
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import {createStyles, makeStyles, Theme, ThemeProvider} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import {Container, CssBaseline} from '@material-ui/core';
+import Head from 'next/head';
+import theme from '../theme';
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -23,11 +25,10 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const MyApp: React.FunctionComponent<AppProps> = ({Component, pageProps}) => {
+const Header: React.FunctionComponent<{ pageProps: any }> = ({pageProps}) => {
   const classes = useStyles();
 
-  return <>
-    <CssBaseline/>
+  return (
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
@@ -41,10 +42,35 @@ const MyApp: React.FunctionComponent<AppProps> = ({Component, pageProps}) => {
         </Typography>
       </Toolbar>
     </AppBar>
-    <Container maxWidth="md">
-      <Component {...pageProps} />
-    </Container>
-  </>;
+  );
+};
+
+
+const MyApp: React.FunctionComponent<AppProps> = ({Component, pageProps}) => {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{pageProps?.title ?? "Mathias Lengler"}</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline/>
+        <Header pageProps={pageProps}/>
+        <Container maxWidth="md">
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,220 +1,75 @@
-import Head from 'next/head'
+import {GetStaticProps} from "next";
+import React from "react";
+import {Project, projects} from "../data/projects";
+import {Button, Card, CardActions, CardContent, CardMedia, Grid, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/styles";
 
-// import {GetStaticPaths, GetStaticProps} from 'next'
-//
-// export const getStaticProps: GetStaticProps = async context => {
-//   return {props: {}};
-// };
-//
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   return {
-//     paths: [
-//       {params: {}}
-//     ],
-//     fallback: false
-//   }
-// };
+interface ProjectsProps {
+  projects: Array<Project>;
+}
 
-// TODO: replace with projects
+export const getStaticProps: GetStaticProps<ProjectsProps> = async () => {
+  return {
+    props: {
+      projects
+    }
+  };
+};
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico"/>
-    </Head>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+const Projects: React.FunctionComponent<ProjectsProps> = (props) => {
+  return (
+    <>
+      <Grid container>
+        {props.projects.map((project, i) => (
+          <Grid item key={i}>
+            <ProjectCard project={project}/>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+export default Projects;
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+interface ProjectCardProps {
+  project: Project
+}
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+const ProjectCard: React.FunctionComponent<ProjectCardProps> = ({project}) => {
+  const classes = useStyles();
 
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo"/>
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-);
-
-export default Home;
+  const demo = project.links.demo;
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={project.teaserImgSrc}
+        title={`Teaser for ${project.title}`}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {project.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          TODO: description
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+        <a href={project.links.github}>GitHub</a>
+        {demo && <a href={demo}>Demo</a>}
+      </CardActions>
+    </Card>
+  );
+};
